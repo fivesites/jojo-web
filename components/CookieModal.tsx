36fe42db561d169cbb1ff1f06b293f0244ea35d2
joeljarvi <joeljarvi@live.se>
2025-12-18 12:54:02 +0100

    fix med cookieModal

--- components/CookieModal.tsx
+++ components/CookieModal.tsx
@@ -9,21 +9,23 @@ export default function CookieModal() {
       initial={{ opacity: 0, y: 24 }}
       animate={{ opacity: 1, y: 0 }}
       exit={{ opacity: 0, y: 24 }}
-      className="fixed bottom-0 left-0 right-0 z-50 flex items-center justify-end p-1 "
+      className="fixed bottom-0 left-0 right-0 z-50 flex items-center justify-end p-1 w-full "
     >
-      <Card className="flex flex-col gap-2 text-sm text-foreground max-w-sm bg-accent p-1 items-start justify-start">
-        <CardDescription className="w-full  font-serif-book pt-6 pb-3 px-6 ">
-          This website uses cookies to enhance the user experience. <br />
-          By continuing to use this website, you agree to our use of cookies.
+      <Card className="w-[90vw] max-w-sm bg-accent p-1">
+        <CardDescription className="font-serif-book pt-6 pb-6 px-6">
+          <p className="whitespace-normal wrap-break-word text-sm">
+            By continuing to browse this site, you agree to the use of cookies
+            to identify your session and to remember your login after you close
+            the browser (authentication cookies).
+          </p>
         </CardDescription>
-        <CardAction className="flex gap-2 items-end justify-end w-full">
-          <Button asChild variant="secondary" size="lg" className="">
+
+        <CardAction className="flex gap-1 justify-end items-end w-full ">
+          <Button asChild variant="secondary" size="lg">
             <Link href="/pages/privacy-policy">Learn more</Link>
           </Button>
 
-          <Button size="lg" className="">
-            Agree & Close
-          </Button>
+          <Button size="lg">Agree & Close</Button>
         </CardAction>
       </Card>
     </motion.div>
